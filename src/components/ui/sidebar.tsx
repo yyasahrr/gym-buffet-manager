94e105319e89e79b486686b07dc47160d0dac408
Yshr Tvkli <yashar4808@gmail.com>
2026-01-02 10:42:13 +0000

    Try fixing this error: `Console Error: Invalid value for prop `as` on <b

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -536,6 +536,7 @@ const sidebarMenuButtonVariants = cva(
 const SidebarMenuButton = React.forwardRef<
   HTMLButtonElement,
   React.ComponentProps<"button"> & {
+    as?: React.ElementType
     asChild?: boolean
     isActive?: boolean
     tooltip?: string | React.ComponentProps<typeof TooltipContent>
@@ -543,6 +544,7 @@ const SidebarMenuButton = React.forwardRef<
 >(
   (
     {
+      as: Comp = 'button',
       asChild = false,
       isActive = false,
       variant = "default",
@@ -553,7 +555,9 @@ const SidebarMenuButton = React.forwardRef<
     },
     ref
   ) => {
-    const Comp = asChild ? Slot : "button"
+    if (asChild) {
+      Comp = Slot
+    }
     const { isMobile, state } = useSidebar()
 
     const button = (
